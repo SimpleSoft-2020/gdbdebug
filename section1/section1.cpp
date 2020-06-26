4e17df9d534a19686e18373bd0b2e4e5f21a2c4b
SimpleSoft <3405187170@qq.com>
2020-06-25 22:50:24 -0400

    update section1

--- section1/section1.cpp
+++ section1/section1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 int main(int argc,char**argv)
 {
+	int itest=100;
+	const char *str="this is a test";
+	std::cout << "itest is " << itest << ",str is " << str << std::endl;
 	std::cout << "参数为:" << std::endl;
 	for(int i=0;i<argc;i++)
 	{
